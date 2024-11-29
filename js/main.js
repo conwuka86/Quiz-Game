d58ad5198a3d0618ffce60bfbe171c16f68e2b7e
conwuka <ogochuku7@gmail.com>
2024-11-29 10:39:25 -0600

    UPDATE AS OF TODAY

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+// main.js
+
+// Global Variables
+let score = 0;
+const winningScore = 3; // Change as needed
+let timer;
+let timeLeft = 10;
+let currentQuestionIndex = 0;
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
